61e94071f8afff58f3c6928487b4839fe161917d
Tim Ruscica <techwithtimm@gmail.com>
2024-03-13 20:59:24 +0400

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,23 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+  baseURL: import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL
+    : "/choreo-apis/npgb/backend-rjz/test-rest-endpoint-5c6/v1.0",
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    }, 
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
