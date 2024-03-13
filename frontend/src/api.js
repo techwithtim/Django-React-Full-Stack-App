d7f1f708cb7188b25fe2c19e9715d9305e4a2346
Tim Ruscica <techwithtimm@gmail.com>
2024-03-13 21:39:31 +0400

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,23 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+
+const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    }, 
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
